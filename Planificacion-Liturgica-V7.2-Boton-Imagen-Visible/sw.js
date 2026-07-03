6d90a0c37adee7473980dfa34cba14813103198c
vectoresdesing18-del <vectoresdesing18@gmail.com>
2026-07-03 05:56:03 -0400

    Add files via upload

---
+++ Planificacion-Liturgica-V7.2-Boton-Imagen-Visible/sw.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = "liturgica-v7-2-shell";
+const SHELL = [
+  "./",
+  "./index.html",
+  "./css/style.css",
+  "./js/app.js",
+  "./js/firebase.js",
+  "./js/calendar.js",
+  "./manifest.webmanifest",
+  "./assets/icon.svg"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(SHELL)).catch(() => {})
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((key) => key !== CACHE_NAME ? caches.delete(key) : null))
+    )
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  const request = event.request;
+
+  if (request.method !== "GET") return;
+
+  event.respondWith(
+    caches.match(request).then((cached) => {
+      if (cached) return cached;
+
+      return fetch(request).catch(() => {
+        if (request.mode === "navigate") {
+          return caches.match("./index.html");
+        }
+      });
+    })
+  );
+});
